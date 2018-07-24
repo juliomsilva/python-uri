932a80b050a9ed6645d6cdcb1c9c9fc6c679e09a
Júlio Silva <toomuch.js@gmail.com>
2018-07-24 16:29:03 -0300

    problem solved: 1006

---
+++ problems/1006.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    A = 2 * float(input())
+    B = 3 * float(input())
+    C = 5 * float(input())
+
+    print("MEDIA = {:.1f}".format((A+B+C) / 10))
+
+
+if __name__ == "__main__":
+    main()
